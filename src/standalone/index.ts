ef5ed77bc41290d966758fd047221eb6cc78e881
RobolabGs2 <slavikelk@yandex.ru>
2021-05-13 01:54:31 +0300

    URL params

--- src/standalone/index.ts
+++ src/standalone/index.ts
@@ -7,17 +7,17 @@ import { Graphics } from "../oddvar/graphics";
 import { Controller } from "../oddvar/controller";
 import { TexturesManager } from "../oddvar/textures";
 import { Oddvar, Worlds } from "../oddvar/oddvar";
-import { GameLogic, Manager } from "../oddvar/manager";
+import { Manager } from "../oddvar/manager";
 import { CollectingSquaresGame, MapCreator, PacManMap, BigPacManMap, TestMap, RandomMap } from '../games/collecting_squares/collecting_squares';
 import { GameMap } from "../games/utils/game_map";
 import { MultiagentSimulation } from '../games/multiagent/simulation';
 import { MonoagentSimulation } from '../games/monoagent/simulation';
 import { Keyboard } from "../oddvar/input";
-import { KeyAction } from "../oddvar/protocol";
 import { HTML } from "../web/html";
 import { Point, Size } from "../oddvar/geometry";
 import { MetricsTable, StyleSheetTree, WindowsManager } from "../web/windows";
 import { Labirint } from "../oddvar/labirint/labirint";
+import { ConvertRecord } from "../oddvar/utils";
 
 console.log("Hello ODDVAR");
 
@@ -45,6 +45,45 @@ const smallMap = Labirint.SymmetryOdd([
 	[0, 0, 1, 1]
 ]).Frame(1);
 
+interface Holder<T> {
+	has(item: T): boolean
+}
+
+type MapOfSets<T> = {
+	[K in keyof T]: Holder<T[K]>
+}
+
+function URIStorage<T extends object>(defaults: T, constraints: MapOfSets<T>): T {
+	function getURL() {
+		return new URL(location.href);
+	}
+	return new Proxy<T>(Object.create(null), {
+		get: (_, field) => {
+			if (typeof field !== "string")
+				return;
+			const constraint = constraints[field as keyof MapOfSets<T>];
+			if (constraint === undefined)
+				return;
+			const url = getURL();
+			const value = (url.searchParams.get(field) || "");
+			if (constraint.has(value as any)) return value;
+			const defaultValue = (defaults as unknown as any)[field];
+			url.searchParams.set(field, defaultValue);
+			history.pushState(null, "", url.toString());
+			return defaultValue;
+		},
+		set: (_, field, value) => {
+			if (typeof field !== "string")
+				return false;
+			const url = getURL();
+			if (url.searchParams.get(field) === value)
+				return true;
+			url.searchParams.set(field, value);
+			history.pushState(null, "", url.toString());
+			return true;
+		},
+	})
+}
 
 Promise.all([DownloadResources(), GetStyleSheet()]).then(([[reflectionJSON, resources], styleSheet]) => {
 	const gameSize = 800;
@@ -80,25 +119,33 @@ Promise.all([DownloadResources(), GetStyleSheet()]).then(([[reflectionJSON, reso
 	const mainWindowsManager = new WindowsManager(mainWindowsContainer, new StyleSheetTree(styleSheet));
 
 	const maps = {
-		symmetric: new GameMap(PacManMap, new Size(gameSize, gameSize)),
-		symmetric_mini: new GameMap(smallMap, new Size(gameSize, gameSize)),
-		symmetric_big: new GameMap(BigPacManMap, new Size(gameSize, gameSize)),
-		test: TestMap,
-		"random maze": new GameMap(RandomMap, new Size(gameSize, gameSize)),
+		symmetric: { name: "Лабиринт", value: new GameMap(PacManMap, new Size(gameSize, gameSize)) },
+		symmetric_mini: { name: "Маленький лабиринт", value: new GameMap(smallMap, new Size(gameSize, gameSize)) },
+		symmetric_big: { name: "Большой лабиринт", value: new GameMap(BigPacManMap, new Size(gameSize, gameSize)) },
+		random_maze: { name: "Случайная карта", value: new GameMap(RandomMap, new Size(gameSize, gameSize)) },
+		test: { name: "Карта для тестов физики", value: TestMap },
 	}
-	type gameCreator = (o: Oddvar, m: MapCreator | GameMap) => GameLogic | undefined;
 	const games = {
-		"Симуляция с кучей агентов": (o: Oddvar, m: MapCreator | GameMap) => (m instanceof GameMap) ? new MultiagentSimulation(o, m, gameWindowsManager) : undefined,
-		"Симуляция с одним агентом": (o: Oddvar, m: MapCreator | GameMap) => (m instanceof GameMap) ? new MonoagentSimulation(o, m, gameWindowsManager) : undefined,
-		"Собери квадраты": (o: Oddvar, m: MapCreator | GameMap) => new CollectingSquaresGame(o, m),
+		multiagent: {
+			name: "Симуляция с кучей агентов",
+			value: (o: Oddvar, m: MapCreator | GameMap) => (m instanceof GameMap) ? new MultiagentSimulation(o, m, gameWindowsManager) : undefined,
+		},
+		monoagent: {
+			name: "Симуляция с одним агентом",
+			value: (o: Oddvar, m: MapCreator | GameMap) => (m instanceof GameMap) ? new MonoagentSimulation(o, m, gameWindowsManager) : undefined,
+		},
+		collecting_squares: {
+			name: "Игра 'Собери квадраты'",
+			value: (o: Oddvar, m: MapCreator | GameMap) => new CollectingSquaresGame(o, m),
+		},
 	}
-	type SimulationSettings = { map: keyof typeof maps, game: keyof typeof games }
-	const url = new URL(location.href);
-	const settings: SimulationSettings = {map:(url.searchParams.get("map")|| "symmetric") as keyof typeof maps, game: (url.searchParams.get("game")|| "Симуляция с кучей агентов") as keyof typeof games}
-	let lastMap: MapCreator | GameMap = maps[settings.map];
-	let lastGame: gameCreator = games[settings.game];
-	
-	const newManager = (game: gameCreator = lastGame, map: MapCreator | GameMap = lastMap) => {
+	type MapID = keyof typeof maps;
+	type GameID = keyof typeof games;
+	type SimulationSettings = { map: MapID, game: GameID }
+
+	const settings = URIStorage<SimulationSettings>({ map: "symmetric", game: "multiagent" }, { map: new Set(Object.keys(maps) as MapID[]), game: new Set(Object.keys(games) as GameID[]) });
+
+	const newManager = (game: GameID = settings.game, map: MapID = settings.map) => {
 		gameWindowsManager.Dispose();
 		const worlds = new Worlds(
 			new World(),
@@ -108,11 +155,12 @@ Promise.all([DownloadResources(), GetStyleSheet()]).then(([[reflectionJSON, reso
 			new Controller(false),
 			new TexturesManager(resources, canvasContext))
 		const oddvar = new Oddvar(worlds, reflectionJSON);
-		lastGame = game;
-		const newGame = game(oddvar, lastMap = map);
+		const newGame = games[game].value(oddvar, maps[map].value);
 		if (newGame === undefined) {
 			throw new Error(`Карта и игра несовместимы`);
 		}
+		settings.game = game;
+		settings.map = map;
 		return new Manager(oddvar, newGame);
 	}
 	const processor = new Processor(newManager(), [
@@ -123,12 +171,12 @@ Promise.all([DownloadResources(), GetStyleSheet()]).then(([[reflectionJSON, reso
 	// keyboards.map((x, i) => mainWindowsManager.CreateInfoWindow(`Player ${i}`, x.joystick(), new Point(i * (gameSize - gameSize / 5), gameSize - 20)));
 	mainWindowsManager.CreateInfoWindow("Настройки", HTML.CreateElement("article", HTML.Append(
 		HTML.CreateElement("article", HTML.SetStyles(style => { style.display = "flex"; style.flexDirection = "row"; }), HTML.Append(([
-			["game", games, (value) => processor.manager = newManager(value, lastMap)],
-			["map", maps, (value) => processor.manager = newManager(lastGame, value)],
-		] as [keyof SimulationSettings, Record<string, any>, (v: any) => void][]).
-			map(([name, record, onChange]) => HTML.CreateElement("section", HTML.Append(
+			["game", games, (value) => processor.manager = newManager(value, undefined)],
+			["map", maps, (value) => processor.manager = newManager(undefined, value)],
+		] as [keyof SimulationSettings, Record<string, { name: string }>, (v: any) => void][]).
+			map(([name, labels, onChange]) => HTML.CreateElement("section", HTML.Append(
 				HTML.CreateElement("header", HTML.SetText(`Choose ${name}:`), HTML.SetStyles(s => s.marginRight = "16px")),
-				CreateSelector(name, settings[name], record, onChange)
+				CreateSelector(settings[name], ConvertRecord(labels, (_, o) => o.name), onChange)
 			))))),
 		HTML.CreateElement("article",
 			HTML.SetStyles(style => {
@@ -153,20 +201,16 @@ Promise.all([DownloadResources(), GetStyleSheet()]).then(([[reflectionJSON, reso
 		))), new Point(gameSize, 0))
 });
 
-function CreateSelector<T>(name: string, defaultKey: string, options: Record<string, T>, onChange: (value: T) => void) {
+function CreateSelector<T extends string>(defaultKey: T, options: Record<T, string>, onChange: (value: T) => void) {
 	return HTML.CreateElement("select",
 		HTML.AddEventListener("change", function () {
 			try {
-				const key = (<HTMLSelectElement>this).value;
-				onChange(options[key])
-				const url = new URL(location.href);
-				url.searchParams.set(name, key)
-				history.pushState(null, "", url.toString());
+				onChange(<T>(<HTMLSelectElement>this).value)
 			} catch (e) {
 				alert(`${e}`)
 			}
 		}),
-		HTML.Append(...Object.keys(options).map(name => HTML.CreateElement("option", HTML.SetText(name), (el) => el.value = name))),
+		HTML.Append(...Object.entries(options).map(([value, text]) => HTML.CreateElement("option", HTML.SetText(text as string), (el) => el.value = value))),
 		el => {
 			el.selectedIndex = Object.keys(options).findIndex(k => k === defaultKey);
 		}
